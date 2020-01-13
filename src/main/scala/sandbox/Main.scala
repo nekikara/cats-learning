7925fc051162a6b08171ee279197dc4a05e8aa46
nekikara <nekikara@gmail.com>
2020-01-13 15:47:14 +0100

    First writer monad

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,42 +1,32 @@
 package sandbox
 
-import cats.Monad
+import cats.data.Writer
+import cats.instances.vector._
+import cats.syntax.applicative._
+import cats.syntax.writer._
 
-import cats.syntax.functor._
-import cats.syntax.flatMap._
-import scala.language.higherKinds
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import cats.instances.future._
 import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-import cats.instances.option._
-import cats.instances.list._
-
-import cats.syntax.applicative._
-
 object Main extends App {
-  type Id[A] = A
+  type Logged[A] = Writer[Vector[String], A]
 
-  val fm = Monad[Future]
-  val future = fm.flatMap(fm.pure(1))(x => fm.pure(x + 2))
-  Await.result(future, 10.second)
+  def slowly[A](body: => A): A =
+    try body finally Thread.sleep(100)
 
-  1.pure[Option]
-  1.pure[List]
-
-  def sumSquare[F[_]: Monad](a: F[Int], b: F[Int]): F[Int] =
+  def factorial(n: Int): Logged[Int] =
     for {
-      x <- a
-      y <- b
-    } yield x * x + y * y
+      slowed <-
+        if (n==0) 1.pure[Logged]
+        else slowly(factorial(n - 1).map(_ * n))
+      _ <- Vector(s"fact ${n} $slowed").tell
+    } yield slowed
 
-  println(sumSquare(Option(3), Option(4)))
-  println(sumSquare(List(1, 2, 3), List(4, 5)))
-  println(sumSquare(3: Id[Int], 4: Id[Int]))
+  val result = Await.result(Future.sequence(Vector(
+    Future(factorial(3).run),
+    Future(factorial(3).run)
+  )), 5.seconds)
 
-  println(CId.pure(123))
-  println(CId.map(123)(_ * 2))
-  println(CId.flatMap(123)(_ * 2))
+  println(result)
 }
