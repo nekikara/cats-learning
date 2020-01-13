5bf64404096d463de457734f1a3386667edf3056
nekikara <nekikara@gmail.com>
2020-01-13 19:14:03 +0100

    First State monad

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,61 +1,87 @@
 package sandbox
 
-import cats.data.Reader
+import cats.data.State
 import cats.syntax.applicative._
 
 object Main extends App {
-  val catName: Reader[Cat, String] =
-    Reader(cat => cat.name)
-  val greetKitty: Reader[Cat, String] =
-    catName.map(name => s"Hello $name")
+  val step1 = State[Int, String] { state =>
+    val ans = state + 1
+    (ans, s"$state + 1 = Result of step1: $ans\n")
+  }
 
-  val feedKitty: Reader[Cat, String] =
-    Reader(cat => s"Have a nice bowl of ${cat.name}")
+  val step2 = State[Int, String] { num =>
+    val ans = num * 2
+    (ans, s"$num * 2 = Result of step2: $ans\n")
+  }
 
-  val greetAndFeed: Reader[Cat, String] =
-    for {
-      greet <- greetKitty
-      feed <- feedKitty
-    } yield s"$greet. $feed"
+  val both = for {
+    a <- step1
+    b <- step2
+  } yield (a, b)
 
-  println(greetAndFeed(Cat("Ken", 35, "blue")))
-  println(greetAndFeed(Cat("Maya", 35, "red")))
+  val (state, result) = both.run(100).value
+//  println(state)
+//  println(result)
 
-  /*
-   * DbReader
-   */
-  case class Db(usernames: Map[Int, String], passwords: Map[String, String])
-  type DbReader[A] = Reader[Db, A]
+  type CalcState[A] = State[List[Int], A]
+  def evalOne(sym: String): CalcState[Int] =
+    sym match {
+      case "+" => operator(_ + _)
+      case "-" => operator(_ - _)
+      case "/" => operator(_ / _)
+      case "*" => operator(_ * _)
+      case num => operand(num.toInt)
+    }
 
-  def findUsername(userId: Int): DbReader[Option[String]] =
-    Reader(db => db.usernames.get(userId))
+  def operand(i: Int): CalcState[Int] = State[List[Int], Int] { state =>
+    (i :: state, i)
+  }
 
-  def checkPassword(username: String, password: String): DbReader[Boolean] =
-    Reader(db => db.passwords.get(username).contains(password))
+  def operator(function: (Int, Int) => Int): CalcState[Int] =
+    State[List[Int], Int] {
+      case b :: a :: tail =>
+        val ans = function(a, b)
+        (ans :: tail, ans)
+      case _ =>
+        sys.error("Fail!")
+    }
 
-  def checkLogin(userId: Int, password: String): DbReader[Boolean] =
-    for {
-      username <- findUsername(userId)
-      result <- username.map { un =>
-                  checkPassword(un, password)
-                }.getOrElse {
-                  false.pure[DbReader]
-                }
-    } yield result
+  def evalAll(input: List[String]): CalcState[Int] =
+    input.foldLeft(0.pure[CalcState]) { (acc, b) =>
+      acc.flatMap(_ => evalOne(b))
+    }
+//    (input: @unchecked) match {
+//      case h :: tail => for {
+//                          b <- evalOne(h)
+//                          ans <- if (tail != Nil) evalAll(tail) else State.pure[List[Int], Int](b)
+//                        } yield ans
+//    }
 
-  val users = Map(
-    1 -> "dade",
-    2 -> "kate",
-    3 -> "margo"
-  )
+  val first = evalOne("42").runA(Nil).value
+  println(first)
 
-  val passwords = Map(
-    "dade" -> "zerocool",
-    "kate" -> "acidburn",
-    "margo" -> "secret"
-  )
-  val db = Db(users, passwords)
-  println("===== Check Login by using checkLogin =====")
-  println(checkLogin(1, "zerocool").run(db))
-  println(checkLogin(3, "secret").run(db))
+  val program = for {
+    _ <- evalOne("1")
+    _ <- evalOne("2")
+    ans <- evalOne("+")
+  } yield ans
+  println(program.runA(Nil).value)
+
+  val program2 = evalAll(List("1", "2", "+", "3", "*"))
+  println(s"program2 => ${program2.runA(Nil).value}")
+
+  val program3 = for {
+    _ <- evalAll(List("1", "2", "+"))
+    _ <- evalAll(List("3", "4", "+"))
+    ans <- evalOne("*")
+  } yield ans
+  println(s"program3 => ${program3.runA(Nil).value}")
+
+  def evalInput(input: String): CalcState[Int] =
+    evalAll(input.split(" ").toList)
+
+  val program4 = evalInput("1 2 + 3 4 + *")
+  println(s"program4 => ${program4.runA(Nil).value}")
+  val program5 = evalInput("1 2 + 3 *")
+  println(s"program5 => ${program5.runA(Nil).value}")
 }
