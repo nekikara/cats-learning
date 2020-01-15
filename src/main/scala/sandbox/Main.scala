e1a3da462974d0b841eda1601c6cc1eb0c65f1c1
nekikara <nekikara@gmail.com>
2020-01-15 06:27:02 +0100

    Myself map flatMap filter & sum

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,77 +1,33 @@
 package sandbox
 
-
 object Main extends App {
-  import cats.data.Validated
-  import cats.instances.list._
-  import cats.syntax.either._
-  import cats.syntax.apply._
+  val left = List(1, 2, 3, 4).foldLeft(List.empty[Int])((acc, i) => i :: acc)
+  println(left)
+  val right = List(1, 2, 3, 4).foldRight(List.empty[Int])((acc, i) => acc :: i)
+  println(right)
 
-  type FormData = Map[String, String]
-  type FailFast[A] = Either[List[String], A]
-  type FailSlow[A] = Validated[List[String], A]
+  def map[A, B](list: List[A])(f: A => B): List[B] =
+    list.foldRight(List.empty[B])((i, acc) => f(i) :: acc)
+  println(map(List(1, 2, 3))(_ * 2))
 
-  def getValue(name: String)(data: FormData): FailFast[String] =
-    data.get(name).toRight(List(s"$name field not specified"))
+  def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] =
+    list.foldRight(List.empty[B])((i, acc) => f(i) ::: acc)
+  println(flatMap(List(1, 2, 3))(a =>  List(a * 2, a * 4, a * 6)))
 
-  def parseInt(str: String): FailFast[Int] =
-    util.Try(str.toInt).toEither match {
-      case Right(x) => Right(x)
-      case Left(_) => Left(List(s"$str can't be parsed"))
-    }
-  def nonBlank(value: String): FailFast[Boolean] =
-    if (value.nonEmpty) Right(true) else Left(List(s"$value is blank!"))
-  def nonNegative(value: Int): FailFast[Boolean] =
-    if (value > 0) {
-      Right(true)
-    } else {
-      Left(List(s"$value is negative"))
-    }
+  def filter[A](list: List[A])(f: A => Boolean): List[A] =
+    list.foldRight(List.empty[A])((i, acc) =>  if (f(i)) i :: acc else acc )
+  println(filter(List(1, 2, 3))(2 <= _))
 
-  def readName(data: FormData): FailFast[String] =
-    for {
-      name <- getValue("name")(data)
-      _ <- nonBlank(name)
-    } yield name
+  import scala.math.Numeric
+  def sum[A](list: List[A])(implicit numeric: Numeric[A]): A =
+    list.foldRight(numeric.zero)(numeric.plus)
 
-  def readAge(data: FormData): FailFast[Int] =
-    for {
-      age <- getValue("age")(data)
-      n <- parseInt(age)
-      _ <- nonNegative(n)
-    } yield n
+  println(sum(List(1, 2, 3)))
 
-  case class User(name: String, age: Int)
+  import cats.Monoid
+  import cats.instances.int._
 
-  def validatedUser(data: FormData): FailSlow[User] =
-    (
-      readName(data).toValidated,
-      readAge(data).toValidated
-    ).mapN(User.apply)
-
-  val validData = Map(
-    "name" -> "hogeohge",
-    "age" -> "32"
-  )
-  println(validatedUser(validData))
-  val blankName = Map(
-    "name" -> "",
-    "age" -> "32"
-  )
-  println(validatedUser(blankName))
-  val negativeAge = Map(
-    "name" -> "",
-    "age" -> "-32"
-  )
-  println(validatedUser(negativeAge))
-  val noNameKey = Map(
-    "nam" -> "",
-    "age" -> "-32"
-  )
-  println(validatedUser(noNameKey))
-  val noNumericAge = Map(
-    "name" -> "sxxx",
-    "age" -> "asdie"
-  )
-  println(validatedUser(noNameKey))
+  def sumWithMonoid[A](list: List[A])(implicit monoid: Monoid[A]): A =
+    list.foldRight(monoid.empty)(monoid.combine)
+  println(sumWithMonoid(List(4, 5, 6)))
 }
